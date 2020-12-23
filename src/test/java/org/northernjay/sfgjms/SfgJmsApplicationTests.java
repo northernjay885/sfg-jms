ace808d48aef2e90ea238b32b14ebdb5e5dd1e3c
luorenjie <lrj193927@gmail.com>
2020-12-23 08:40:25 -0500

    init project

---
+++ src/test/java/org/northernjay/sfgjms/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.northernjay.sfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
